b3abeccbb68f324b82b2c566087d7c4eac473b54
Marco Di Gennaro <marco@Marcos-Air.fritz.box>
2024-04-09 09:25:35 +0200

    first commit from MBA

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='WindML',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies your package needs
+    ],
+)
+
